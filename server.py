f168f0e8e0110282f9b6d90aa71542f7b3f4c812
parham-server <parhamserver2010@gmail.com>
2025-07-22 19:48:04 +0400

    Update server.py

--- server.py
+++ server.py
@@ -17,6 +17,21 @@ def send_message():
     else:
         return jsonify({"status": "error", "message": "user not specified or message invalid"}), 400
 
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    data = request.json
+    user = data.get("user")
+    
+    if user in messages:
+        del messages[user]
+        return jsonify({"status": "deleted", "user": user})
+    else:
+        return jsonify({"status": "not found", "user": user}), 404
+
+
+
+
 @app.route('/rece', methods=['GET'])
 def get_messages():
     return jsonify(messages)
